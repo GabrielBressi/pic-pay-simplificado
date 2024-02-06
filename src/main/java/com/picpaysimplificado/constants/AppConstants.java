97531cf59cee1248fba2bec3d0984caae1d6a58d
GabrielBressi <gabrielbressi18@gmail.com>
2024-02-06 17:58:36 -0300

    :recycle: refactor: created AppConstants

---
+++ src/main/java/com/picpaysimplificado/constants/AppConstants.java
@@ -0,0 +1,9 @@
+package com.picpaysimplificado.constants;
+
+public class AppConstants {
+
+    public static final String MESSAGE = "message";
+    public static final String EMPTY = "";
+    public static final String ATHORIZED_TRANSACTION = "Autorizado";
+
+}
