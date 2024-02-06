c8bd24fcf4d006c9cbbb27a81c9e67a0978e5185
GabrielBressi <gabrielbressi18@gmail.com>
2024-02-06 13:48:24 -0300

    :tada: initial commit

---
+++ src/main/java/com/picpaysimplificado/PicPaySimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.picpaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySimplificadoApplication.class, args);
+	}
+
+}
