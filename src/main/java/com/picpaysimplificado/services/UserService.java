927391b24760276ed79befc757064e1eea33bb00
GabrielBressi <gabrielbressi18@gmail.com>
2024-02-06 15:44:23 -0300

    :sparkles: feat: added UserService logic

--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -1,2 +1,34 @@
-package com.picpaysimplificado.services;public class UserService {
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.models.users.UserModel;
+import com.picpaysimplificado.models.users.UserType;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(UserModel payer, BigDecimal amount) throws Exception {
+        if(payer.getUserType() == UserType.SHOPKEEPER) {
+            throw new Exception("Usuário do tipo logista não está autorizado a realizar transação.");
+        }
+
+        if(payer.getBalance().compareTo(amount) < 0) {
+            throw new Exception(String.format("Usuário id:%s não possui saldo suficiente", payer.getUserId().toString()));
+        }
+    }
+
+    public UserModel findUserById(Long userId) throws Exception {
+        return this.userRepository.findUserByUserId(userId).orElseThrow(() -> new Exception("Usuário não encontrado."));
+    }
+
+    public void saveUser(UserModel user) {
+        this.userRepository.save(user);
+    }
 }
