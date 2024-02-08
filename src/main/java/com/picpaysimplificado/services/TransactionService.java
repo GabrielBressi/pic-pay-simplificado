96ce0811c5966689aa3336fce1f6e0b562176b18
GabrielBressi <gabrielbressi18@gmail.com>
2024-02-08 00:26:02 -0300

    :recycle: refactor: injected AthorizationService

--- src/main/java/com/picpaysimplificado/services/TransactionService.java
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -6,16 +6,11 @@ import com.picpaysimplificado.models.users.UserModel;
 import com.picpaysimplificado.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Map;
 
-import static com.picpaysimplificado.constants.AppConstants.*;
 
 @Service
 public class TransactionService {
@@ -27,13 +22,10 @@ public class TransactionService {
     private TransactionRepository transactionRepository;
 
     @Autowired
-    private RestTemplate restTemplate;
-
-    @Autowired
     private NotificationService notificationService;
 
-    @Value("${transaction.checker}")
-    private String transactionChecker;
+    @Autowired
+    private AuthorizationService authorizationService;
 
     public TransactionModel createTransaction(TransactionDTO transaction) throws Exception {
         UserModel payer = this.userService.findUserById(transaction.payer_id());
@@ -41,7 +33,7 @@ public class TransactionService {
 
         userService.validateTransaction(payer, transaction.value());
 
-        boolean isAuthorized = this.authorizeTransaction(payer, transaction.value());
+        boolean isAuthorized = this.authorizationService.authorizeTransaction(payer, transaction.value());
         if(!isAuthorized) {
             throw new Exception("Transação não autorizada");
         }
@@ -53,17 +45,6 @@ public class TransactionService {
         this.notificationService.sendNotification(payee, "Transação recebida com sucesso");
 
         return transactionModel;
-    }
-
-    public boolean authorizeTransaction(UserModel user, BigDecimal value) {
-      ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity(transactionChecker, Map.class);
-
-      if(authorizationResponse.getStatusCode() == HttpStatus.OK) {
-          String message = (String) authorizationResponse.getBody().get(MESSAGE) != null ? (String) authorizationResponse.getBody().get(MESSAGE) : EMPTY;
-          return authorizationResponse.getBody().get(MESSAGE).equals(ATHORIZED_TRANSACTION);
-      }
-
-      return false;
     }
 
     public TransactionModel populateAndSaveTransaction(UserModel payer, UserModel payee, BigDecimal value) {
