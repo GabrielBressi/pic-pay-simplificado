5016581537380e96c8801ad939dd10d6a0636610
GabrielBressi <gabrielbressi18@gmail.com>
2024-02-09 01:33:19 -0300

    :recycle: refactor: removed unnecessary imports

--- src/main/java/com/picpaysimplificado/services/TransactionService.java
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -11,8 +11,6 @@ import com.picpaysimplificado.repositories.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
